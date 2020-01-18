777414823fddf2e11bb8191a90cc74442457ed88
Rafael Santos do Nascimento <rafael.nascimento@adm.unip.br>
2020-01-18 20:19:16 -0300

    First Commit

---
+++ DemoMVCViewComponent/WebApplication1/ViewComponents/CarrinhoViewController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoMVCViewComponent.ViewComponents
+{
+
+    [ViewComponent(Name = "Carrinho")]
+    public class CarrinhoViewController : ViewComponent
+    {
+
+        public int QuantidadeItensCarrinho { get; set; }
+
+        public CarrinhoViewController()
+        {
+            QuantidadeItensCarrinho = 5;
+        }
+
+       public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(QuantidadeItensCarrinho);
+        }
+    }
+}
